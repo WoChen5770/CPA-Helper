ac439c767fed273d7bc28738645eee1a5746944e
WoChen5770 <jaycq.com@gmail.com>
2026-07-01 15:41:49 +0800

    🐛 fix: 移除不需要的数据库迁移文件
    - 删除 202607010001_add_base_path.sql
    - 恢复 LatestVersion 为 202606040001
    - 修复数据库版本不匹配导致的启动失败

--- backend/migrations/202607010001_add_base_path.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE app_settings ADD COLUMN base_path VARCHAR(200) NOT NULL DEFAULT '';
-ALTER TABLE app_settings ADD COLUMN cpamc_url VARCHAR(500) NOT NULL DEFAULT '';
-
--- +goose Down
-ALTER TABLE app_settings DROP COLUMN cpamc_url;
-ALTER TABLE app_settings DROP COLUMN base_path;
